8886fbc6e1fef457119859709281e3f791ccb484
hitomi <1670957065@qq.com>
2016-10-11 13:55:41 +0800

    Initial commit

---
+++ aslibrary/src/main/java/com/hitomi/aslibrary/ActivityControllerLayout.java
@@ -0,0 +1,23 @@
+package com.hitomi.aslibrary;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * Created by hitomi on 2016/10/11.
+ */
+public class ActivityControllerLayout extends FrameLayout {
+
+    public ActivityControllerLayout(Context context) {
+        super(context);
+    }
+
+    public ActivityControllerLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ActivityControllerLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
---
+++ aslibrary/src/main/java/com/hitomi/aslibrary/ActivitySwitcherHelper.java
@@ -0,0 +1,15 @@
+package com.hitomi.aslibrary;
+
+import android.support.annotation.NonNull;
+import android.view.Window;
+
+/**
+ * Created by hitomi on 2016/10/11.
+ */
+public class ActivitySwitcherHelper {
+
+    public ActivitySwitcherHelper(@NonNull Window window) {
+
+    }
+
+}
