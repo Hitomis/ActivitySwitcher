2344e4750dad7085990cf963f121c199838a06f7
hitomi <1670957065@qq.com>
2016-11-03 16:37:22 +0800

    Make  view bounds

--- aslibrary/src/main/java/com/hitomi/aslibrary/ActivityContainer.java
+++ aslibrary/src/main/java/com/hitomi/aslibrary/ActivityContainer.java
@@ -1,14 +1,21 @@
 package com.hitomi.aslibrary;
 
 import android.content.Context;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 /**
  * Created by hitomi on 2016/11/2.
  */
 public class ActivityContainer extends FrameLayout {
+
+    private RectF bounds;
+
+    private float offsetX, offsetY;
+    private float tranX, tranY;
 
     private boolean intercept;
 
@@ -22,6 +29,7 @@ public class ActivityContainer extends FrameLayout {
 
     public ActivityContainer(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        bounds = new RectF();
     }
 
     @Override
@@ -29,7 +37,72 @@ public class ActivityContainer extends FrameLayout {
         return intercept;
     }
 
+    @Override
+    public void setLayoutParams(ViewGroup.LayoutParams params) {
+        super.setLayoutParams(params);
+        bounds.left = 0;
+        bounds.top = 0;
+        bounds.right = params.width;
+        bounds.bottom = params.height;
+    }
+
+    @Override
+    public void setScaleX(float scaleX) {
+        super.setScaleX(scaleX);
+        int width = getLayoutParams().width;
+        float halfScaleX = width * (1 - scaleX) * .5f;
+        bounds.left += halfScaleX - offsetX;
+        bounds.right -= halfScaleX - offsetX;
+        offsetX = halfScaleX;
+    }
+
+    @Override
+    public void setScaleY(float scaleY) {
+        super.setScaleY(scaleY);
+        int height = getLayoutParams().height;
+        float halfScaleY = height * (1 - scaleY) * .5f;
+        bounds.top += halfScaleY - offsetY;
+        bounds.bottom -= halfScaleY - offsetY;
+        offsetY = halfScaleY;
+    }
+
+    @Override
+    public void setTranslationX(float translationX) {
+        super.setTranslationX(translationX);
+        bounds.left += translationX - tranX;
+        bounds.right += translationX - tranX;
+        tranX = translationX;
+    }
+
+    @Override
+    public void setTranslationY(float translationY) {
+        super.setTranslationY(translationY);
+        bounds.top += translationY - tranY;
+        bounds.bottom += translationY - tranY;
+        tranY = translationY;
+    }
+
+    @Override
+    public void setX(float x) {
+        super.setX(x);
+        bounds.left += x - tranX;
+        bounds.right += x - tranX;
+        tranX = x;
+    }
+
+    @Override
+    public void setY(float y) {
+        super.setY(y);
+        bounds.top += y - tranY;
+        bounds.bottom += y - tranY;
+        tranY = y;
+    }
+
     public void setIntercept(boolean intercept) {
         this.intercept = intercept;
+    }
+
+    public RectF getBounds() {
+        return bounds;
     }
 }
