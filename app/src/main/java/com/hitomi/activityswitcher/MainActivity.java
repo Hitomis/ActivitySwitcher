6bfef7703f11da59fbfbc2a3c00fdc7a9f763d21
hitomi <1670957065@qq.com>
2016-11-03 17:31:41 +0800

    Modify the sample code

--- app/src/main/java/com/hitomi/activityswitcher/MainActivity.java
+++ app/src/main/java/com/hitomi/activityswitcher/MainActivity.java
@@ -1,22 +1,34 @@
-package com.hitomi.activityswitcher.ui;
+package com.hitomi.activityswitcher;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import com.hitomi.activityswitcher.R;
 import com.hitomi.aslibrary.ActivitySwitcher;
 
 public class MainActivity extends AppCompatActivity {
 
     static int index;
     static int totalCount = 8;
+    static int[] bgColors = new int[] {
+            Color.parseColor("#92c8d0"),
+            Color.parseColor("#c4dcce"),
+            Color.parseColor("#cd7b91"),
+            Color.parseColor("#e5c5dc"),
+            Color.parseColor("#742a8d"),
+            Color.parseColor("#2eb2d8"),
+            Color.parseColor("#b9d84e"),
+            Color.parseColor("#35fe62")
+    };
 
+    private RelativeLayout relativeLayout;
     private Button btnNext;
     private TextView tvPage;
 
@@ -29,9 +41,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         activitySwitcher = ActivitySwitcher.getInstance();
 
+        relativeLayout = (RelativeLayout) findViewById(R.id.relayout);
         btnNext = (Button) findViewById(R.id.btn_next);
         tvPage = (TextView) findViewById(R.id.tv_page);
 
+        relativeLayout.setBackgroundColor(bgColors[index]);
         tag = index;
         tvPage.setText("当前第" + (tag + 1) + "页" );
 
